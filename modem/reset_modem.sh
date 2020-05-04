d71faf584461f87f193932cb7d86ba9a5acb2f7d
Elm Klod <elmklod@tuta.io>
2020-05-04 22:47:34 +0300

    another

--- modem/reset_modem.sh
+++ modem/reset_modem.sh
@@ -1,4 +1,5 @@
 # put it into ~/.config
 # it requires libusb, libusb-devel, tcl, usb-modeswitch, usb-modeswitch-data
+# run # udevadm control --reload-rules
 sudo usb_modeswitch -v 12d1 -p 1f01 -J
 
