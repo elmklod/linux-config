e842a9de9e5178ce7cfb20cfb5a27674508850fd
Klod Elm <waitonthefloor@gmail.com>
2020-08-02 11:11:27 +0300

    Made redshift autostart when xinit is run, deleted now redundant def_layout.sh

--- put-where-belongs.sh
+++ put-where-belongs.sh
@@ -17,7 +17,6 @@ fi
 
 cp --preserve=all $PWD/keyboard/00-keyboard.conf /usr/share/X11/xorg.conf.d/
 cp --preserve=all $PWD/modem/reset_modem.sh $HOME/Tools/
-cp --preserve=all $PWD/screen/def_layout.sh $HOME/Tools/
 cp --preserve=all $PWD/screen/redshift.conf $HOME/.config/
 cp --preserve=all $PWD/screen/.Xdefaults $HOME/
 cp --preserve=all $PWD/screen/.xinitrc $HOME/
